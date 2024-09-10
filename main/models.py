c25593a8ccbe7d6b2a73829e1ff5e10db24d5027
athallah210505 <n4dhif@gmail.com>
2024-09-10 11:05:54 +0700

    correct models.py

--- main/models.py
+++ main/models.py
@@ -5,6 +5,15 @@ class MoodEntry(models.Model):
     time = models.DateField(auto_now_add=True)
     feelings = models.TextField()
     mood_intensity = models.IntegerField()
+    price = models.IntegerField()
+    gold_name = models.CharField(max_length=255)
+    quantity = models.IntegerField()
+    description = models.TextField()
+    name = models.CharField(max_length=255)
+    npm = models.CharField(max_length=255)
+    kelas = models.CharField(max_length=255)
+    
+    
 
     @property
     def is_mood_strong(self):
