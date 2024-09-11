78e23cca8cb9587b0ca76b2fe2e5b306edefeb7b
athallah210505 <n4dhif@gmail.com>
2024-09-11 14:59:31 +0700

    melakukan checking terhadap github karena error pada tutorial

--- main/models.py
+++ main/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 
-class MoodEntry(models.Model):
+class Project(models.Model):
     mood = models.CharField(max_length=255)
     time = models.DateField(auto_now_add=True)
     feelings = models.TextField()
