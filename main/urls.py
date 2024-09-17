8d842d7dcd7448802bdc8d7d4f903f4983eaff5d
athallah210505 <n4dhif@gmail.com>
2024-09-17 13:31:38 +0700

    commit changes on html

--- main/urls.py
+++ main/urls.py
@@ -1,12 +1,12 @@
 from django.urls import path
-from main.views import show_main, create_project_entry, show_xml,show_json, show_xml_by_id, show_json_by_id
+from main.views import show_main, create_product_entry, show_xml,show_json, show_xml_by_id, show_json_by_id
 from . import views
 
 app_name = 'main'
 
 urlpatterns = [
     path('', views.show_main, name='show_main'),
-    path('create_project_entry', create_project_entry, name='create_project_entry'),
+    path('create_product_entry', create_product_entry, name='create_product_entry'),
     path('xml/', show_xml, name='show_xml'),
     path ('json/', show_json, name='show_json'),
     path('xml/<str:id>/', show_xml_by_id, name='show_xml_by_id'),
