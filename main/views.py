8eb78a482ff2878d81175775405ff2f257adf3d1
athallah210505 <n4dhif@gmail.com>
2024-09-23 14:10:43 +0700

    menyelasikan Tugas 4

--- main/views.py
+++ main/views.py
@@ -3,28 +3,41 @@ from main.models import Product
 from main.forms import ProductEntryForm
 from django.http import HttpResponse
 from django.core import serializers
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth import authenticate, login
+from django.contrib.auth import logout
+from django.contrib.auth.decorators import login_required
+import datetime
+from django.http import HttpResponseRedirect
+from django.urls import reverse
 
 # Create your views here.
+@login_required(login_url='/login')
 def show_main(request):
-    product_entries = Product.objects.all()
+    product_entries = Product.objects.filter(user=request.user)
     context = {
         'gold_name' : 'Gold 24K',
         'description': '24K Gold is pure gold. Even though it is called pure gold, 100% purity is unattainable, therefore “pure gold” refers to gold that is between 99.7% and 99.9% gold. Also known as fine gold, pure gold hasn’t been made stronger with other metals, therefore it maintains its rich yellow hue and is a lot softer than other types of gold. Because of this (and because it is so expensive), 24 karat gold jewelry is very rare in the United States. It is, however, a lot more popular in China, India and other Asian countries—especially for investment purposes. ',
         'quantity': '1',
         'price' : 'Rp.10.000.000',
-        'name' : 'Athallah Nadhif',
+        'name' : request.user.username,
         'npm' : '2306275651',
         'kelas' : 'PBP B',
-        'product_entries': product_entries
+        'product_entries': product_entries,
+        'last_login': request.COOKIES['last_login'],
     }
     return render(request,"main.html", context)
 
 def create_product_entry(request):
         form = ProductEntryForm(request.POST or None)
+        
         if form.is_valid() and request.method == "POST":
-            form.save()
-            print(request.POST)
-            return redirect("main:show_main")
+            product_entry = form.save(commit=False)
+            product_entry.user = request.user
+            product_entry.save()
+            return redirect('main:show_main')
         context = {
             "form": form
         }
@@ -46,3 +59,37 @@ def show_xml_by_id(request, id):
 def show_json_by_id(request, id):
     data_id = Product.objects.filter(pk=id)  # Use the id argument passed to the function
     return HttpResponse(serializers.serialize("json", data_id), content_type="application/json")
+
+def register(request):
+    form = UserCreationForm()
+
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Your account has been successfully created!')
+            return redirect('main:login')
+    context = {'form':form}
+    return render(request, 'register.html', context)
+
+def login_user(request):
+   if request.method == 'POST':
+      form = AuthenticationForm(data=request.POST)
+
+      if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+            response = HttpResponseRedirect(reverse("main:show_main"))
+            response.set_cookie('last_login', str(datetime.datetime.now()))
+            return response
+
+   else:
+    form = AuthenticationForm(request)
+    context = {'form': form}
+    return render(request, 'login.html', context)
+
+def logout_user(request):
+    logout(request)
+    response = HttpResponseRedirect(reverse('main:login'))
+    response.delete_cookie('last_login')
+    return response
